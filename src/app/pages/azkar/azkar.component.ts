6edf3795afea59d365eebdfd341e2889c0008a85
mohamed mohran <mohamed01098187312@gmail.com>
2025-04-27 17:34:29 +0300

    fix: fix localization issue

--- src/app/pages/azkar/azkar.component.ts
+++ src/app/pages/azkar/azkar.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { ZkarButtonComponent } from "../../components/zkar-button/zkar-button.component";
-import { ZkarCardComponent } from "../../components/zkar-card/zkar-card.component";
 
 @Component({
   selector: 'app-azkar',
-  imports: [ZkarButtonComponent, ZkarCardComponent],
+  imports: [],
   templateUrl: './azkar.component.html',
   styleUrl: './azkar.component.scss'
 })
